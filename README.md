8fbabc51e743ead128920f27e533946bd243d2b6
Jiří Dvořák <info@dvorakj.com>
2021-02-26 04:20:57 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+An extension for Adobe Brackets editor
+# External changes dialog
+
+DESCRIPTION: Adds a status bar visibility switch to the View menu
+
+VERSION: Tested in Feb 2021 for Brackets Editor of version 1.14.2 on Windows 10
+
+STORY: I created this because I don’t use it much. It is my second plugin.
+
+FUNCTIONALITY: I don’t know, I mean it contains some event listeners which
+watches the preferences and then performs hide/show function of status
+bar module.
+
+AUTHOR:
+Graphic designer
+& amateur fullstack developer
+
+Jiří Dvořák
---
+++ main.js
@@ -0,0 +1,64 @@
+define( function( require, exports, module )
+{
+	"use strict";
+
+	// Modules
+	var app_init		= brackets.getModule( "utils/AppInit" );
+	var cm_manager		= brackets.getModule( "command/CommandManager" );
+	var e_manager		= brackets.getModule( "editor/EditorManager" );
+	var menus			= brackets.getModule( "command/Menus" );
+	var prefs_manager	= brackets.getModule( "preferences/PreferencesManager" );
+	var statusbar		= brackets.getModule( "widgets/StatusBar" );
+
+	// Action
+	app_init.appReady( function()
+	{
+		// Settings
+		var cm_id		= "starboy.hideStatusBar.toggle";
+		var prefs_id	= "starboy.hideStatusBar";
+		var prefs_def	= { checked: true };
+
+		// Declaration
+		var prefs, cm;
+
+		// Action
+		function e_action()
+		{
+			if( cm.getChecked())	statusbar.hide();
+			else					statusbar.show();
+		}
+
+		// Event handlers
+		function cm_handler()
+		{
+			if( !cm.getChecked())	cm.setChecked( true );
+			else					cm.setChecked( false );
+		}
+		function state_handler()
+		{
+			prefs.set( "checked", Boolean( cm.getChecked()));
+			e_action();
+		}
+		function e_handler()
+		{
+			e_action();
+		}
+
+		// Preferences
+		prefs = prefs_manager.getExtensionPrefs( prefs_id );
+		prefs.definePreference( "checked", "boolean", prefs_def[ "checked" ]);
+
+		// Command
+		cm = cm_manager.get( cm_id );
+		if ( !cm )	cm = cm_manager.register( "Hide status bar", cm_id, cm_handler );
+		else		cm._commandFn = cm_handler;
+		$( cm ).on( "checkedStateChange", state_handler );
+		cm.setChecked( prefs.get( "checked" ));
+
+		// Menu
+		menus.getMenu( "view-menu" ).addMenuItem( cm_id );
+
+		// Sync
+		$( e_manager ).on( "activeEditorChange", e_handler );
+	});
+});
